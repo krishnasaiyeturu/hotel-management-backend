4b874f5473e674b9598ada3642da6f946f1dc018
krishnasaiyeturu <krishnasaiyeturu@gmail.com>
2024-10-06 13:46:57 +0530

    added rooms type api

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bookingsController = require('../controller/BookingController');
 
-// // Check room availability
-// router.get('/rooms/availability', bookingsController.checkRoomAvailability);
 
 // Create a booking
+router.get('/', bookingsController.getBookingDetails);
 router.post('/', bookingsController.createBooking);
 router.post('/check-availability', bookingsController.checkAvailability);
-// router.get('/', bookingsController.getBookingDetails);
 router.post('/calculate-total-price', bookingsController.calculateTotalPrice);
 router.put('/check-in/:bookingId', bookingsController.UpdateCheckIn);
 router.get('/:bookingId', bookingsController.getBookingById);
